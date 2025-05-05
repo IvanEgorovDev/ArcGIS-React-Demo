705f2532c0f75d5b757ef3b5ecf7f3c2a1644da2
Ivan Egorov <ivan.egorov.dev@gmail.com>
2025-05-05 14:42:32 +0200

    rebuild dist

--- dist/assets/operatorsWorkerConnection-B8_kyQXF.js
+++
@@ -1,1 +0,0 @@
-import{cc as n}from"./index-BlXvYXyk.js";let t,o,a=!1;function p(){return t??(t=n("arcadeGeometryOperatorsWorker").then(e=>{o=e,a=!0,t=void 0}))}async function c(e,r){return a?o.apply("invokeGeometryOp",[e,r]):(await p(),c(e,r))}export{c as a};
